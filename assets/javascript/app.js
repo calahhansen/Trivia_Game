563e68fe4f2c36825fd6b11c1a3ece7468602618
Calah Hansen <calah.hansen@gmail.com>
2019-08-07 22:09:39 -0500

    dang! I think I need arrays now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
 function unhide() {
-    document.getElementById("panel").style.display = "block";
+  document.getElementById("quiz").style.display = "block";
+}
+
+let timeleft = 120;
+let downloadTimer = setInterval(function(){
+  document.getElementById("time").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished"
   }
+}, 1000);
